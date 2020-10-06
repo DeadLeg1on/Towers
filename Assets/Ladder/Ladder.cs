9fc5675b8b539b8275c1213588284d006bcc74d8
Stas <shuharenkostas@gmail.com>
2020-10-06 20:22:44 +0300

    new

---
+++ Assets/Ladder/Ladder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+    float speed = 5;
+    float last_x, last_y;
+    KeyCode buttonJump = KeyCode.Space;
+    void OnTriggerEnter2D (Collider2D other) { }
+    void OnTriggerStay2D (Collider2D other) {
+        if (other.gameObject.CompareTag ("Player")) {
+            if (Input.GetKey (buttonJump)) {
+                other.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, speed);
+                //other.transform.position = new Vector2 (transform.position.x + 0.7f, other.transform.position.y);
+
+            }
+
+        }
+        if (Input.GetKeyUp (buttonJump)) {
+            //other.transform.position = new Vector2 (transform.position.x + 0.7f, -transform.position.y);
+            other.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+        }
+    }
+    void OnTriggerExit2d (Collider2D other) {
+        if (other.gameObject.CompareTag ("Player")) {
+            if (other.transform.position.y > transform.position.y) {
+                GetComponent<BoxCollider2D> ().isTrigger = false;
+            }
+        }
+    }
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    int start_posX;
+    int start_posY;
+    int last_posX;
+    int last_posY;
+
+    [SerializeField]
+    private float jumpForce = 5;
+
+    [SerializeField]
+    private float speed = 1;
+
+    [SerializeField]
+    public bool isJump = false;
+    [SerializeField]
+    float sprite_size = 3;
+    [SerializeField]
+    float direction; //1 - right -1 - left
+
+    [SerializeField]
+    bool ladderUp = false;
+
+    Rigidbody2D rig2d;
+    SpriteRenderer sprite;
+
+    void Awake () {
+        direction = sprite_size;
+        rig2d = GetComponent<Rigidbody2D> ();
+        sprite = GetComponent<SpriteRenderer> ();
+    }
+    void Start () {
+        Run ();
+    }
+    void Update () {
+        if (isJump == false && ladderUp == false) {
+            if (Input.GetKeyDown (KeyCode.Space) || Input.GetKey (KeyCode.Joystick1Button3)) {
+                rig2d.AddForce (transform.up * jumpForce, ForceMode2D.Impulse);
+                isJump = true;
+
+            }
+            if (Input.GetKeyDown (KeyCode.A)) {
+                direction = -1 * sprite_size;
+                Run ();
+            }
+            if (Input.GetKeyDown (KeyCode.D)) {
+                direction = 1 * sprite_size;
+                Run ();
+            }
+        }
+    }
+
+    private void Run () {
+        rig2d.velocity = new Vector2 (speed * direction, rig2d.velocity.y);
+        transform.localScale = new Vector3 (direction, sprite_size, sprite_size);
+    }
+    void OnCollisionEnter2D (Collision2D other) {
+        if (other.gameObject.tag == "Platform") {
+            isJump = false;
+        }
+        if (other.gameObject.tag == "Ladder") {
+            isJump = false;
+        }
+    }
+    void OnCollisionExit2D (Collision2D other) {
+        if (other.gameObject.tag == "Ladder") {
+            isJump = true;
+        }
+    }
+
+}
